3522f7f10a4de86642272109e3bd4f3671075e84
emortaldev <me@emortal.dev>
2024-04-05 02:32:17 +0100

    Make it look nice

--- index.js
+++ index.js
@@ -1,16 +1,33 @@
+console.log("😱")
+
+const quota = document.getElementById("quotaInput");
+const totalSold = document.getElementById("totalSoldInput");
+const deadlineDays = document.getElementById("deadlineDaysInput")
+
+quota.addEventListener("input", checkValue);
+totalSold.addEventListener("input", checkValue);
+deadlineDays.addEventListener("input", checkValue);
+
+checkValue()
+totalSold.focus()
+
+function checkValue() {
+    quota.value = quota.value.replace(/[^0-9]/g,"");
+    totalSold.value = totalSold.value.replace(/[^0-9]/g,"");
+
+    quota.size = Math.max(1, quota.value.toString().length);
+    totalSold.size = Math.max(1, totalSold.value.length);
+
+    deadlineDays.value = Math.min(Math.max(deadlineDays.value, 0), 3);
+
+    calculateQuota();
+}
+
 function calculateQuota() {
-    let quota = document.getElementById("quotaInput").value;
-    let totalSold = document.getElementById("totalSoldInput").value;
-    let deadlineDays = document.getElementById("deadlineDaysInput").value;
-    if (deadlineDays === 0) deadlineDays = -1;
+    let deadlineDaysVal = deadlineDays.value;
+    if (deadlineDaysVal === 0) deadlineDaysVal = -1;
 
-    let overtimeBonus = Math.round((totalSold - quota) / 5 + 15 * deadlineDays);
-
-    if (overtimeBonus <= 0) {
-        document.getElementById("overtime").innerHTML = "You get no overtime bonus!"
-    } else {
-        document.getElementById("overtime").innerHTML = overtimeBonus;
-    }
-
-
+    let overtimeBonus = Math.max(0, Math.round((totalSold.value - quota.value) / 5 + 15 * deadlineDaysVal));
+    if (isNaN(overtimeBonus)) return;
+    document.getElementById("overtime").innerHTML = "$" + overtimeBonus;
 }
